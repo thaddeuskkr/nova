c97e716354372523e676c8865be84cefeaf58cba
Thaddeus Kuah <tk@tkkr.dev>
2024-08-22 00:15:17 +0800

    feat(api): improve error messages and add application information
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- src/routes/*.ts
+++ src/routes/*.ts
@@ -11,19 +11,29 @@ export const routes: Route = (fastify, { config }, done) => {
                 reply.code(404).send('This short URL does not exist. ' +
                 'If you believe that this is an error, please contact the owner of the URL.' +
                 '\n\n' +
-                `${config.info.name} v${config.info.version} by ${config.info.author}`);
+                `${config.info.name} v${config.info.version} by ${config.info.author}` +
+                '\n' +
+                'https://github.com/thaddeuskkr/nova');
                 return;
             }
             if (link.public) reply.code(301).redirect(link.url);
             else {
                 const query = request.query as { password?: string; pw?: string; pass?: string };
                 if (!query || (!query.password && !query.pw && !query.pass)) {
-                    reply.code(401).send('This URL is password protected and you did not provide one in query parameters');
+                    reply.code(401).send('This URL is password protected and you did not provide one in query parameters' +
+                    '\n\n' +
+                    `${config.info.name} v${config.info.version} by ${config.info.author}` +
+                    '\n' +
+                    'https://github.com/thaddeuskkr/nova');
                     return;
                 }
                 const password = query.password || query.pw || query.pass;
                 if (password !== link.password) {
-                    reply.code(401).send('Incorrect password');
+                    reply.code(401).send('Incorrect password' +
+                    '\n\n' +
+                    `${config.info.name} v${config.info.version} by ${config.info.author}` +
+                    '\n' +
+                    'https://github.com/thaddeuskkr/nova');
                     return;
                 }
                 reply.code(301).redirect(link.url);
